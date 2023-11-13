49a1b8ede75bf989325110f304154fdd8e713157
Noha Shehab <63148220+NohaAShehab@users.noreply.github.com>
2023-11-13 12:30:07 +0200

    intro to python

---
+++ intro.py
@@ -0,0 +1,104 @@
+
+
+name = 'Ahmed'
+print(name)
+grade = 85
+
+"""
+    if(grade >=85){
+        cout << "Excellent" << endl;
+    }
+
+"""
+
+# if grade>=85:
+#     print("Excellent student ")
+
+bio= 'My name is Noha'
+bio2= "I lives in Cairo"
+
+print(bio)
+print(bio2)
+
+bio3=("My name is Noha"
+      " I works at ITI")
+
+# print(bio3)
+
+""" multi-line string """
+
+# bio4= """My name is Noha
+# I works at ITI
+# I lives in cairo"""
+# print(bio4)
+
+"you can use this string as comment"
+
+# this a comment
+
+# print='Ali'
+#
+# print("test")
+
+""" get types of variables """
+
+name='noha'
+print(type(name))
+num = 10  # int
+""" casting the variables"""
+num = str(num)
+print(type(num))
+
+year ='2023'
+year = float(year)
+print(year, type(year))
+
+# name='n'
+# name=int(name)
+
+"""operators"""
+# num =10
+# print(num**3)
+# num **=4
+
+
+"====> operators <=========="
+
+print(True and False)
+
+# print("Ahmed" and 'Mohamed')
+#
+# print(bool('ahmed'))
+# print(bool('Mohamed'))
+
+
+print("iti" and 0)
+
+
+print(bool(' '))
+# print("iti" and '')
+
+
+print("Ahmed" and "ali" and 13 and 'test')
+
+print('' and'Ahmed' and 'iti' and 'test')
+
+print(bool("Shrouk"))
+
+print(bool(('' and'Ahmed' and 'iti' and 'test')))
+
+
+if 'shrouk':
+    print("hiii")
+else:
+    print("byyeee")
+
+
+
+
+
+
+
+
+
+
